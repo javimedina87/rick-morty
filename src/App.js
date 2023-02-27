22c09fb35c8ea15aa9540c981c697ef9e19a3d94
Javier Medina Lopez <franciscojavier.medina.lopez@bbvaitspain.com>
2023-02-27 10:42:00 +0100

    Change router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import CharacterContainer from './components/Pages/CharacterContainer/CharacterContainer';
 import RootLayout from './components/RootLayout/RootLayout';
 import HomePage from './components/Pages/HomePage/HomePage';
 import ErrorPage from "./components/Pages/ErrorPage/ErrorPage";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout/>,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/', element: <HomePage/> },
-      { path: '/characters', element: <CharacterContainer/> }
-    ]
-  },
-])
-
 function App() {
   return (
-    <RouterProvider router={router}/>
-  );
+    <Routes>
+      <Route path='/' element={<RootLayout/>} errorElement={<ErrorPage/>}>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/characters' element={<CharacterContainer/>}/>
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <HashRouter>
+    <App/>
+  </HashRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
